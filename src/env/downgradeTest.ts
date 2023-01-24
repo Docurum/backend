6ba00b9cf4f5772a87edefb6cc0159c22cf73298
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2023-01-24 06:15:35 +0530

    🗑️ Upgraded JS files to TS

--- src/env/downgradeTest.ts
+++ src/env/downgradeTest.ts
@@ -1,11 +1,11 @@
-const fs = require("fs").promises;
+import { promises as fs } from "fs";
 
-const setEnvValue = async (key, value) => {
+const setEnvValue = async (key: string, value: string): Promise<void> => {
   // read file from hdd & split if from a linebreak to a array
   const ENVS = await fs.readFile("./.env", "utf8");
   const ENV_VARS = ENVS.split("\n");
   // find the env we want based on the key
-  const target = ENV_VARS.indexOf(ENV_VARS.find((line) => line.match(new RegExp(key))));
+  const target = ENV_VARS.indexOf(ENV_VARS.find((line) => line.match(new RegExp(key))) as string);
   // replace the key/value with the new value
   ENV_VARS.splice(target, 1, `${key}=${value}`);
   // write everything back to the file system
@@ -13,4 +13,5 @@ const setEnvValue = async (key, value) => {
   console.log(`Updated ${key} to ${value}`);
 };
 
-setEnvValue("DATABASE_URL", "${TEST_DB_URL}");
+// eslint-disable-next-line @typescript-eslint/no-floating-promises, no-template-curly-in-string
+setEnvValue("DATABASE_URL", "${DEV_DB_URL}");

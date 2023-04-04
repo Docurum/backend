093eb42572bd38d145eee8f47646b272ec86a31b
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2023-04-05 03:25:17 +0530

    fixed get use rdate

--- src/v1/controllers/user/User.controller.ts
+++ src/v1/controllers/user/User.controller.ts
@@ -38,6 +38,10 @@ const userController = {
           username: true,
           picture: true,
           isDoctor: true,
+          bio: true,
+          dob: true,
+          phoneNumber: true,
+          
         },
       });
       res.json(customResponse(200, user));
